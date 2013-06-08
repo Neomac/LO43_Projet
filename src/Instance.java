9e28191494cf381ee52e92c97ac3f400e4e3c3ba
toptere <arnaudv@mac.com>
2013-06-08 17:26:54 +0200

    Commiting test 2

---
+++ src/Instance.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+//Commit Test 2
+
+public class Instance {
+	
+	protected static ArrayList<Tache> tachesInstance = new ArrayList<Tache>();
+		
+	public static void LectureInstance(){
+			try {
+				FileInputStream fstream = new FileInputStream("Instance_1.txt");
+				DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				String strLine;
+				
+				while((strLine = br.readLine()) != null){
+					StringTokenizer st = new StringTokenizer(strLine, " ");
+					Tache NouvelleTache = new Tache(Integer.parseInt(st.nextToken())*60+Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())*60+Integer.parseInt(st.nextToken()), st.nextToken(), st.nextToken());
+					tachesInstance.add(NouvelleTache);
+				}
+				in.close();
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Erreur de lecture du fichier d'instance!");
+			}
+			
+		}
+	public static void PrintArray(){
+		for (int i=0; i<tachesInstance.size(); i++){
+			(tachesInstance.get(i)).PrintTache();
+		}
+	}
+}
---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+//Commit Test 2
+
+public class Solution {
+	private int coutTotal;
+	protected static ArrayList<Chauffeur> Chauffeurs = new ArrayList<Chauffeur>();
+	
+	
+	public static void LectureSolution(){
+		try {
+			FileInputStream fstream = new FileInputStream("Solution_1.txt");
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			
+			while((strLine = br.readLine()) != null){
+				StringTokenizer st = new StringTokenizer(strLine, " ");
+				System.out.println(st.nextToken());
+				//Chauffeur nouveauChauffeur = new Chauffeur();
+				//Chauffeurs.add(nouveauChauffeur);
+			}
+			in.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Erreur de lecture du fichier d'instance!");
+		}
+	}
+}
