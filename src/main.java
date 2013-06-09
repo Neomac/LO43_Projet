1ba8301f44ef7c8e35d49849f9e76b5104e9c572
toptere <arnaudv@mac.com>
2013-06-09 18:26:12 +0200

    Finalisation de la methode LectureSolution

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+
 public class main {
 
   /**
@@ -5,21 +6,32 @@ public class main {
    */
   public static void main(String[] args) {
     
+    
     System.out.println("RŽsultat de la lecture du fichier:");
+    
     Fichier testFichier = new Fichier();
     testFichier.LectureInstance();
     testFichier.PrintArray();
     
+	  
+    /*String fichierSolution = "Solution_4.txt";
+    System.out.println("Lecture du fichier solution");
     Solution testSolution = new Solution();
-    testSolution.LectureSolution();
-    
+    testSolution.LectureSolution(fichierSolution);
+    testSolution.PrintSolution();
+    */
+	  
     //Test commit 2
     //Coucou c'est nous les dopidop !!!
     
     //Test commit 2
     //Test commit 2
     
-    //Test Top
+    //Test Top !
+    //Test Top Test
+    //Test Top Test
+    //Test Top Test
+    //Test Top Test
     
   }
 
