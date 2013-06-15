e46898e6d4466736850fecc8e47d9ee002907998
Simon <simon.pageaud@gmail.com>
2013-06-15 18:57:09 +0200

    Interface 2.0+

--- src/Cadre.java
+++ src/Cadre.java
@@ -10,7 +10,7 @@ import javax.swing.JTextArea;
 
 public class Cadre {
 	public Cadre(){}
-	
+	private JTextArea Texte;
 	/*
 	public Cadre(String titre, int width, int height){
 		JPanel temp = new JPanel();
@@ -41,12 +41,14 @@ public class Cadre {
         temp.setBackground(Color.white);
         temp.setPreferredSize(new Dimension(width,height));
         temp.setBorder(BorderFactory.createTitledBorder(titre));
-        JTextArea texte = new JTextArea(); 
-        texte.setEditable(false);
-        temp.add(texte);
+        //JTextArea texte = new JTextArea();
+        Texte = new JTextArea();
+        Texte.setEditable(false);
+        temp.add(Texte);
         temp.setVisible(true);
         return temp;
 	}
+	
 	public JPanel CreerCadre_2(String titre, int width, int height){
 		JPanel temp = new JPanel();
 		temp.setBackground(Color.white);
@@ -59,4 +61,26 @@ public class Cadre {
         temp.setVisible(true);
         return temp;
 	}
+	
+	public JPanel CreerCadre_scrollpane(String titre, int width, int height){
+		JPanel temp = new JPanel();
+        temp.setBackground(Color.white);
+        temp.setPreferredSize(new Dimension(width,height));
+        temp.setBorder(BorderFactory.createTitledBorder(titre));
+        //JTextArea texte = new JTextArea();
+        Texte = new JTextArea(30,15);
+        Texte.setEditable(false);
+        temp.add(new JScrollPane(Texte));
+        temp.setVisible(true);
+        return temp;
+	}
+	
+	public JTextArea getTexte() {
+		return Texte;
+	}
+	
+	public void setTexte(JTextArea texte) {
+		Texte = texte;
+	}
+	
 }
