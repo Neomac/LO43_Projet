1683a9396d8b83f6f189374f03b95ae74bdbe1e5
Neomac <antoine18@gmail.com>
2013-06-15 13:07:52 +0200

    Modification accent main

--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ public class main {
   public static void main(String[] args) {
     
     
-    System.out.println("R�sultat de la lecture du fichier:");
+    System.out.println("Résultat de la lecture du fichier:");
     
     //Fichier testFichier = new Fichier();
     //testFichier.LectureInstance();
