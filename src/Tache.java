5a2f201bf483b6a9d5022f35667c87b1757ea71f
toptere <arnaudv@mac.com>
2013-06-20 23:01:18 +0200

    Commentaires ajoutés à Tache.java

--- src/Tache.java
+++ src/Tache.java
@@ -2,18 +2,19 @@ import java.math.*;
 
 public class Tache {
 	private int numeroTache;
-	private int numeroChauffeur;
+	private int numeroChauffeur;				//Numéro du chauffeur auquel la tache est assignée, par défaut à 0
 	private int HeureDepart; 	//En minutes
 	private int HeureArrivee;	//En minutes
 	private String LieuDepart;	
 	private String LieuArrivee;
 	
-
+	//Constructeur par défaut de Tache
 	public Tache(){
-		numeroChauffeur=HeureDepart = HeureArrivee = 0;
+		numeroChauffeur=numeroChauffeur=HeureDepart = HeureArrivee = 0;
 		LieuDepart = LieuArrivee = "";
 	}
 	
+	//Constructeur de Tache
 	public Tache(int _numeroTache,int _numeroChauffeur, int _heureDepart, int _heureArrivee, String _lieuDepart, String _lieuArrivee) {
 		numeroTache = _numeroTache;
 		numeroChauffeur = _numeroChauffeur;		//Mettre à 0 si un chauffeur n'est pas attribué
@@ -23,22 +24,27 @@ public class Tache {
 		LieuArrivee = _lieuArrivee;
 	}
 	
+	//Imprime les attributs de la Tache dans la console
 	public void PrintTache(){
 		System.out.println("Task:"+getNumeroTache()+"\tstartTime:"+getHeureDepart()+"\tfinishTime:"+getHeureArrivee()+getLieuDepart()+getLieuArrivee());
 	}
 	
-	public String StringTacheChauffeur(){
-		return "Task:"+getNumeroTache()+"\tChauffeur"+getNumeroChauffeur()+"\tstartTime:"+getHeureDepart()+"\tfinishTime:"+getHeureArrivee()+getLieuDepart()+getLieuArrivee()+"\n";
-	}
-	
+	//Renvoie sous forme de String les mêmes informations que la fonction PrintTache, utilisé pour afficher dans l'interface graphique
 	public String StringTache(){
 		return "Task:"+getNumeroTache()+"\tstartTime:"+getHeureDepart()+"\tfinishTime:"+getHeureArrivee()+getLieuDepart()+getLieuArrivee()+"\n";
 	}
 	
+	//Idem que StringTache mais renvoie en plus le numéro du Chauffeur assigné à la Tache
+	public String StringTacheChauffeur(){
+		return "Task:"+getNumeroTache()+"\tChauffeur"+getNumeroChauffeur()+"\tstartTime:"+getHeureDepart()+"\tfinishTime:"+getHeureArrivee()+getLieuDepart()+getLieuArrivee()+"\n";
+	}
+	
+	//Renvoie le Chauffeur associé à la Tache de la solution
 	public Chauffeur getChauffeurTache(Solution solution){
 		return solution.getChauffeur(this.numeroChauffeur-1);
 	}
 	
+	//Assesseurs de Tache
 	public int getNumeroTache() {
 		return numeroTache;
 	}
